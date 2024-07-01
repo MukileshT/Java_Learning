6c4ef595718dfddd7c9d648e26ba693c4faa4090
Mukilesh <145214507+MukileshT@users.noreply.github.com>
2024-07-01 20:35:41 +0530

    Update calculator.java
    added closing action for window

--- calculator.java
+++ calculator.java
@@ -1,9 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
 
-class calculator implements ActionListener {
+class calculator extends WindowAdapter implements ActionListener {
     Frame f = new Frame();
-    TextField t =new TextField();
+    TextField t = new TextField();
     Button b0 = new Button("0");
     Button b1 = new Button("1");
     Button b2 = new Button("2");
@@ -23,11 +23,11 @@ class calculator implements ActionListener {
     Button be = new Button("=");
     Button bp = new Button(".");
     Button bE = new Button("Exit");
-       
-    double number1, number2, result;
-    String operation;
 
-    calculator(){
+    double number1, number2, result;
+    char operation;
+
+    public calculator() {
         f.add(t);
         f.add(b0);
         f.add(b1);
@@ -68,7 +68,8 @@ class calculator implements ActionListener {
         be.addActionListener(this);
         bp.addActionListener(this);
         bE.addActionListener(this);
-        
+        f.addWindowListener(this);
+
         t.setBounds(5, 30, 396, 70);
         b7.setBounds(0, 102, 60, 60);
         b8.setBounds(60, 102, 60, 60);
@@ -79,95 +80,99 @@ class calculator implements ActionListener {
         b1.setBounds(0, 222, 60, 60);
         b2.setBounds(60, 222, 60, 60);
         b3.setBounds(120, 222, 60, 60);
-        b0.setBounds(0, 282, 180, 60); 
+        b0.setBounds(0, 282, 180, 60);
         bp.setBounds(180, 282, 60, 60);
         bs.setBounds(180, 102, 60, 60);
-        be.setBounds(240, 282, 60, 60); 
+        be.setBounds(240, 282, 60, 60);
         ba.setBounds(180, 162, 60, 60);
         bm.setBounds(180, 222, 60, 60);
         bd.setBounds(240, 102, 60, 60);
         bc.setBounds(240, 162, 60, 60);
         bce.setBounds(240, 222, 60, 60);
-        bE.setBounds(290, 470, 100, 20);       
-    
+        bE.setBounds(290, 470, 100, 20);
+
         f.setLayout(null);
         f.setSize(400, 500);
         f.setVisible(true);
-        }   
-        
+    }
+
+    public void windowClosing(WindowEvent e) {
+        f.dispose();
+    }
+
     public void actionPerformed(ActionEvent e) {
-        if(e.getSource() == bE){
+        if (e.getSource() == bE) {
             System.exit(0);
         }
-        if(e.getSource() == b1){
-            t.setText(t.getText()+1); 
+        if (e.getSource() == b1) {
+            t.setText(t.getText() + 1);
         }
-        if(e.getSource() == b2){
-            t.setText(t.getText()+2);
+        if (e.getSource() == b2) {
+            t.setText(t.getText() + 2);
         }
-        if(e.getSource() == b3){
-            t.setText(t.getText()+3);
+        if (e.getSource() == b3) {
+            t.setText(t.getText() + 3);
         }
-        if(e.getSource() == b4){
-            t.setText(t.getText()+4);
+        if (e.getSource() == b4) {
+            t.setText(t.getText() + 4);
         }
-        if(e.getSource() == b5){
-            t.setText(t.getText()+5);
+        if (e.getSource() == b5) {
+            t.setText(t.getText() + 5);
         }
-        if(e.getSource() == b6){
-            t.setText(t.getText()+6);
+        if (e.getSource() == b6) {
+            t.setText(t.getText() + 6);
         }
-        if(e.getSource() == b7){
-            t.setText(t.getText()+7);
+        if (e.getSource() == b7) {
+            t.setText(t.getText() + 7);
         }
-        if(e.getSource() == b8){
-            t.setText(t.getText()+8);
+        if (e.getSource() == b8) {
+            t.setText(t.getText() + 8);
         }
-        if(e.getSource() == b9){
-            t.setText(t.getText()+9);
+        if (e.getSource() == b9) {
+            t.setText(t.getText() + 9);
         }
-        if(e.getSource() == b0){
-            t.setText(t.getText()+0);
+        if (e.getSource() == b0) {
+            t.setText(t.getText() + 0);
         }
-        if(e.getSource() == ba){
+        if (e.getSource() == ba) {
             number1 = Double.parseDouble(t.getText());
             t.setText("");
-            operation = "+";
-        }     
-        if(e.getSource() == bs){
+            operation = '+';
+        }
+        if (e.getSource() == bs) {
             number1 = Double.parseDouble(t.getText());
             t.setText("");
-            operation = "-";
+            operation = '-';
         }
-        if(e.getSource() == bm){
+        if (e.getSource() == bm) {
             number1 = Double.parseDouble(t.getText());
             t.setText("");
-            operation = "*";
+            operation = '*';
         }
-        if(e.getSource() == bd){
+        if (e.getSource() == bd) {
             number1 = Double.parseDouble(t.getText());
             t.setText("");
-            operation = "/";
+            operation = '/';
         }
-        if(e.getSource() == bp){
+        if (e.getSource() == bp) {
             t.setText("");
         }
-        if(e.getSource() == be){
+        if (e.getSource() == be) {
             number2 = Double.parseDouble(t.getText());
             switch (operation) {
-                case "+":
-                    t.setText(String.valueOf(number1+number2));
+                case '+':
+                    t.setText(String.valueOf(number1 + number2));
                     break;
-                case "-":
-                    t.setText(String.valueOf(number1-number2));
+                case '-':
+                    t.setText(String.valueOf(number1 - number2));
                     break;
-                case "*":
-                    t.setText(String.valueOf(number1*number2));
+                case '*':
+                    t.setText(String.valueOf(number1 * number2));
                     break;
-                case "/":
-                    if(number2 != 0){
-                        t.setText(String.valueOf(number1/number2));
-                    }else{
+                case '/':
+                    if (number2 != 0) {
+                        t.setText(String.valueOf(number1 / number2));
+                    } else {
                         t.setText("Division by zero is not posible");
                     }
                     break;
@@ -175,15 +180,20 @@ class calculator implements ActionListener {
                     break;
             }
         }
-        if(e.getSource() == bc){
+        if (e.getSource() == bce) {
+            String a = t.getText();
             t.setText("");
+            for (int i = 0; i < a.length() - 1; i++) {
+                t.setText(t.getText() + a.charAt(i));
+            }
         }
-        if(e.getSource() == bce){
+        if (e.getSource() == bc) {
             number1 = 0;
             number2 = 0;
             t.setText("");
         }
-    } 
+    }
+
     public static void main(String[] args) {
         new calculator();
     }
